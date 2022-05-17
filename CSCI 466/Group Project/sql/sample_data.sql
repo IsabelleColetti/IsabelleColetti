7b75914dec4d031666059dde4e84b7088acdda5a
Isabelle Coletti <92697125+IsabelleColetti@users.noreply.github.com>
2022-05-17 12:33:33 -0500

    Add files via upload

---
+++ CSCI 466/Group Project/sql/sample_data.sql
@@ -0,0 +1,414 @@
+/**
+ * @file sample_data.sql
+ * 
+ * @brief Provides sample data for the database.
+ * 
+ * @author David Petrovski
+ * @author Isabelle Coletti
+ * @author Amanda Zedwick
+ * 
+ * CSCI 466 - 1
+ */
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Dog Food',
+	'Give your best friend all the energy and nutrition they need to live life to the fullest. This formula includes all the necessary vitamins and minerals for your pooch, with a delicious beef flavor.',
+	'25.50',
+	'500',
+	'https://images2.imgbox.com/e7/74/OhlW7DXn_o.png'
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Food Bowl',
+	'The highest quality stainless steel pet food bowl on the market. Includes an anti-microbial surface to keep the germs away from your precious pet.',
+	'7.00',
+	'250',
+	'https://images2.imgbox.com/02/fb/bqz3I1Jp_o.png'
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'A Very Special Shampoo',
+	'The most special shampoo formula ever created, with secret ingredient aloe to nourish your pet''s skin. The packaging is also 100% recyclable, so it is healthy for pets, people, and the planet.',
+	'21.55',
+	'250',
+	'https://images2.imgbox.com/f1/d9/CyLrmKd2_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(	
+	'Collar (Small)',
+	'A chew-proof, olive green collar made from nylon with an easy side release buckle. The diameter measures 4 inches across. Recommended for small dog breeds or large cats.',
+	'8',
+	'300',
+	'https://images2.imgbox.com/6f/e9/2nuYpCIw_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Collar (Large)',
+	'A heavy-duty black and tan leather collar. Includes a traditional buckle for security and decorative gold plating. Diameter measures 7 inches across. Ideal for large dog breeds.',
+	'10',
+	'300',
+	'https://images2.imgbox.com/b3/07/2IjAqifZ_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(	
+	'Leash',
+	'Keep your pet with you at all times with this 7-foot-long sturdy, black nylon leash. Can be used with dogs or cats up to 80 pounds.',
+	'12.99',
+	'400',
+	'https://images2.imgbox.com/0f/e3/T4jkbFKB_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(	
+	'Food Mat',
+	'Give your buddy the best eating experience possible with this food mat. Not only is it an amazing anti-slip surface for underneath their bowl, it catches any spills and keeps your floor clean!',
+	'28.99',
+	'429',
+	'https://images2.imgbox.com/52/f2/3TxdsyIy_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Pet Bed (Small)',
+	'This brown, circular pet bed is designed with the comfort of your pet in mind. The fluffy interior provides warmth while the shape allows your dog or cat to curl up comfortably inside. Measures 2 feet in diameter.',
+	'99.99',
+	'300',
+	'https://images2.imgbox.com/6d/e4/iI6K6W96_o.png'
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(	
+	'Pet Bed (Large)',
+	'This fun cow-print rectangular ped is perfect for your large pet. Memory foam inside eases their joints, and the large surface area provides plenty of room to relax. The cover is removable and machine washable. Measures 4 feet by 3 feet.',
+	'150.99',
+	'350',
+	'https://images2.imgbox.com/9b/e8/m8Ib3y5E_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Brush',
+	'A double-sided brush with high quality wooden handle. Perfect for all coats. Designed to fit comfortably and ergonomically in your hand as you groom your furry friend.',
+	'9.99',
+	'400',
+	'https://images2.imgbox.com/98/af/LzKtEsvQ_o.png'
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Pet Cologne',
+	'This cologne will keep your male furry friend smelling great for weeks! The scent is inspired by a rainforest, with hints of eucalyptus and light citrus. Made with aloe to nourish your pet''s coat.',
+	'20.99',
+	'500',
+	'https://images2.imgbox.com/c9/2d/KgMapJ2g_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Pet Perfume',
+	'This perfume will freshen up your female furry friend for weeks! The scent is vanilla almond. Formula includes oatmeal to nourish your pet''s coat.',
+	'25.99',
+	'400',
+	'https://images2.imgbox.com/1d/89/oRhBNPYl_o.png'
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Logo Pet Hoodie (Small)',
+	'A gray pet hoodie with the Bob''s logo printed on. Recommended for small dog breeds and cats. Machine washable.',
+	'30.50',
+	'100',
+	'https://images2.imgbox.com/9a/d4/FpheE4Yi_o.png'
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(	
+	'Logo Pet Hoodie (Large)',
+	'A gray pet hoodie with the Bob''s logo printed on. Recommended for large dog breeds. Machine washable.',
+	'35.50',
+	'150',
+	'https://images2.imgbox.com/bb/73/qgLXC5PE_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(	
+	'Hot Dog Toy (Squeaky)',
+	'Meet your best friend''s new favorite toy! This squeaky hot dog is perfect to keep them occupied, especially during the summer time. Designed to endure chewing and slobbering.',
+	'69.99',
+	'600',
+	'https://images2.imgbox.com/40/05/HArcYloD_o.png'
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(	
+	'Tarantula',
+	'A large, adult male tarantula ready to be your next best friend! May vary in size, between 5 and 7 inches including legspan. Guaranteed live delivery.',
+	'150.00',
+	'10',
+	'https://images2.imgbox.com/4a/27/7SdP91xU_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Chinchilla',
+	'An adult female chinchilla, ready to provide lots of love! May vary in size, between 9 and 14 inches. Guaranteed live delivery.',
+	'200.00',
+	'12',
+	'https://images2.imgbox.com/75/94/xdIsSm6a_o.png'
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Baby Fox',
+	'An energetic baby fox! Gender may vary, along with size and coloring. Guaranteed live delivery. May grow to be 8 to 15 inches tall.',
+	'499.99',
+	'13',
+	'https://images2.imgbox.com/84/1b/TpaehIoJ_o.png' 
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(
+	'Chameleon',
+	'A colorful male chameleon! This camoflauging friend will be the perfect addition to your household! May vary in size, between 12 and 20 inches. Guaranteed live delivery.',
+	'299.99',
+	'5',
+	'https://images2.imgbox.com/87/94/2hgIpDhh_o.png'
+);
+
+
+INSERT INTO Products
+(Name, Description, Price, Quantity, ImgLink)
+VALUES
+(	
+	'Bearded Dragon (Bob-Made)',
+	'This bearded dragon is specially Bob-made, with Bob''s proprietary formulas. Size may vary between 12 and 19 inches, along with gender. Guaranteed live delivery.',
+	'999.99',
+	'6',
+	'https://images2.imgbox.com/eb/03/cR7fFMle_o.png' 
+);
+
+
+INSERT INTO Employees
+VALUES
+(
+	'admin',
+	'admin',
+	PASSWORD('12345678')
+);
+
+
+INSERT INTO Customers
+VALUES
+(
+	'admin',
+	PASSWORD('12345678'),
+	'admin',
+	'admin@niu.cs.edu'
+);
+
+
+INSERT INTO Customers
+VALUES
+(
+	'DavidP',
+	PASSWORD('password'),
+	'David',
+	'petrovskidavid1@gmail.com'
+);
+
+
+INSERT INTO Customers
+VALUES
+(
+	'AmandaZ',
+	PASSWORD('password'),
+	'Amanda',
+	'amanda@amandamail.com'
+);
+
+
+INSERT INTO Customers
+VALUES
+(
+	'IzzyC',
+	PASSWORD('password'),
+	'Isabelle',
+	'izzy@izzymail.com'
+);
+
+
+INSERT INTO Customers
+VALUES
+(
+	'BobO',
+	PASSWORD('password'),
+	'Bob',
+	'bob@bobmail.com'
+);
+
+
+INSERT INTO Orders
+(OrderID, Username, Total, Status, Address)
+VALUES
+(
+	'1247',
+	'admin',
+	'42.55',
+	'2',
+	"1425 W Lincoln Hwy, DeKalb, IL 60115"
+);
+
+INSERT INTO Carts
+VALUES
+(
+	'1247',
+	'1',
+	'admin',
+	'1'
+);
+
+
+INSERT INTO Orders
+(OrderID, Username, Total, Status, Address)
+VALUES
+(
+	'1248',
+	'IzzyC',
+	'89.31',
+	'2',
+	"1425 W Lincoln Hwy, DeKalb, IL 60115"
+);
+
+INSERT INTO Carts
+VALUES
+(
+	'1248',
+	'15',
+	'IzzyC',
+	'1'
+);
+
+
+INSERT INTO Orders
+(OrderID, Username, Total, Status, Address)
+VALUES
+(
+	'1249',
+	'BobO',
+	'315.27',
+	'2',
+	"1425 W Lincoln Hwy, DeKalb, IL 60115"
+);
+
+INSERT INTO Carts
+VALUES
+(
+	'1249',
+	'16',
+	'BobO',
+	'2'
+);
+
+
+INSERT INTO Orders
+(OrderID, Username, Total, Status, Address)
+VALUES
+(
+	'1250',
+	'DavidP',
+	'525.44',
+	'2',
+	'1425 W Lincoln Hwy, DeKalb, IL 60115'
+);
+
+INSERT INTO Carts
+VALUES
+(
+	'1250',
+	'18',
+	'DavidP',
+	'1'
+);
+
+INSERT INTO Orders
+(OrderID, Username, Total, Status, Address)
+VALUES
+(
+	'1251',
+	'AmandaZ',
+	'47.81',
+	'2',
+	'1425 W Lincoln Hwy, DeKalb, IL 60115'
+);
+
+INSERT INTO Carts
+VALUES
+(
+	'1251',
+	'13',
+	'AmandaZ',
+	'1'
+);
